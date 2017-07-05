e99168b682f56e28015cec635c44440a2e70a57c
Moulik Adak <Moulik.adak@gmail.com>
2017-07-05 16:57:00 +0530

    Update game.cpp

--- My Codes/game.cpp
+++ My Codes/game.cpp
@@ -3,82 +3,54 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <time.h>
-#define MAX 50
+#define MAX 60
 #define INIT 1000
 using namespace std;
 
 int find_no_of_neighbours(int a[][MAX],int i,int j){
-int count =0;
+	int count =0;
 
-if(i<MAX-1)
-{
-	if(a[i+1][j]==1||a[i+1][j]==3)
-	count++;
-if(j<MAX-1)
-	if(a[i+1][j+1]==1||a[i+1][j+1]==3)
-	count++;
-}
-if(j<MAX-1)
-{
-	if(a[i][j+1]==1||a[i][j+1]==3)
-	count++;
-if(i>0)
-	if(a[i-1][j+1]==1||a[i-1][j+1]==3)
-	count++;
-}
-if(i>0){
-	if(a[i-1][j]==1||a[i-1][j]==3)
-	count++;
-if(j>0)
-	if(a[i-1][j-1]==1||a[i-1][j-1]==3)
-	count++;
-}
-if(j>0){
-	if(a[i][j-1]==1||a[i][j-1]==3)
-	count++;
-if(i<MAX-1)
-	if(a[i+1][j-1]==1||a[i+1][j-1]==3)
-	count++;
-}
-
-//
-/*
-if(a[(i+1)%MAX][j]==1||a[(i+1)%MAX][j]==3)
-	count++;
-if(a[i][(j+1)]==1||a[i][(j+1)]==3)
-	count++;
-if(a[i-1][j]==1||a[i-1][j]==3)
-	count++;
-if(a[i][j-1]==1||a[i][j-1]==3)
-	count++;
-
-if(a[(i+1)%MAX][(j+1)]==1||a[(i+1)%MAX][(j+1)]==3)
-	count++;
-if(a[i-1][j-1]==1||a[i-1][j-1]==3)
-	count++;
-if(a[(i+1)%MAX][j-1]==1||a[(i+1)%MAX][j-1]==3)
-	count++;
-if(a[i-1][(j+1)]==1||a[i-1][(j+1)]==3)
-	count++;
-*/
-return count;
-}
-int add(int a[][MAX]){
-	int x,y;
-	cout<<"Enter x & y coordinate :";
-	cin>>x>>y;
-	a[x][y]=1;
+	if(i<MAX-1)
+	{
+		if(a[i+1][j]==1||a[i+1][j]==3)
+		count++;
+	if(j<MAX-1)
+		if(a[i+1][j+1]==1||a[i+1][j+1]==3)
+		count++;
+	}
+	if(j<MAX-1)
+	{
+		if(a[i][j+1]==1||a[i][j+1]==3)
+		count++;
+	if(i>0)
+		if(a[i-1][j+1]==1||a[i-1][j+1]==3)
+		count++;
+	}
+	if(i>0){
+		if(a[i-1][j]==1||a[i-1][j]==3)
+		count++;
+	if(j>0)
+		if(a[i-1][j-1]==1||a[i-1][j-1]==3)
+		count++;
+	}
+	if(j>0){
+		if(a[i][j-1]==1||a[i][j-1]==3)
+		count++;
+	if(i<MAX-1)
+		if(a[i+1][j-1]==1||a[i+1][j-1]==3)
+		count++;
+	}
+	return count;
 }
 
 int add(int a[][MAX],int x,int y){
-//	int x,y;
-//	cout<<"Enter x & y coordinate :";
-//	cin>>x>>y;
 	a[x][y]=1;
 }
+
 int pregnant(int a[][MAX],int x,int y){
 	a[x][y]=2;
 }
+
 int birth_of_babies(int a[][MAX]){
 	int count=MAX;
 	for (int i = 0; i < count; ++i)
@@ -90,6 +62,7 @@ int birth_of_babies(int a[][MAX]){
 		}
 	}
 }
+
 int death_of_elders(int a[][MAX]){
 	int count=MAX;
 	for (int i = 0; i < count; ++i)
@@ -114,9 +87,10 @@ void view(int a[][MAX]){
 		cout<<endl;
 	}
 }
+
 int implant(int a[][MAX]){
 	int option;
-	cout<<"Insert options : \n 1. Add Glider\n";
+	cout<<"Insert options : \n 1. Add Glider\n 2. Randomize\n 3. Add 45 deg\n 4. Add M\n 5. Add circle\n";
 	cin>>option;
 	int X=MAX-10;
 	if(option==1){
@@ -126,15 +100,140 @@ int implant(int a[][MAX]){
 		add(a,3,X+7);
 		add(a,3,X+8);
 	}
+
+	else if(option==2){
+		for (int i = 0; i < MAX; ++i)
+		{
+			for (int j = 0; j < MAX; ++j)
+			{
+				if(rand() % 2 )
+					add(a,i,j);
+			}
+		}
+	}
+
+	else if(option==3){
+		int aa=25,bb=24,xx=0,yy=0;
+
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+		int x=2;
+		add(a,aa - xx+x,aa - yy+x);
+		add(a,bb - xx+x,aa - yy+x);
+		add(a,aa - xx+x,bb - yy+x);
+		add(a,bb - xx+x,bb - yy+x);
+	}
+
+	else if(option==4){
+
+		int aa = 23,bb=24,xx=4,yy=-9;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+
+		aa = 23,bb=24,xx=-9,yy=-9;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=-9,yy= 2;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=-9,yy= -3;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=-9,yy= 6;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=-9,yy=-9;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+		
+		aa = 23,bb=24,xx=4,yy= 2;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=4,yy= -3;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=4,yy= 6;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+
+		aa = 23,bb=24,xx=1,yy=-6;
+
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+		int x=2;
+		add(a,aa - xx+x,aa - yy-x);
+		add(a,bb - xx+x,aa - yy-x);
+		add(a,aa - xx+x,bb - yy-x);
+		add(a,bb - xx+x,bb - yy-x);
+
+		aa= 28,bb=27,xx=0,yy=0;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+		x=2;
+		add(a,aa - xx+x,aa - yy+x);
+		add(a,bb - xx+x,aa - yy+x);
+		add(a,aa - xx+x,bb - yy+x);
+		add(a,bb - xx+x,bb - yy+x);
+
+		add(a,1,X+7);
+		add(a,2,X+6);
+		add(a,3,X+6);
+		add(a,3,X+7);
+		add(a,3,X+8);
+
+	}
+	else if(option==5){
+		int aa=25,bb=24,xx=0,yy=0;
+		add(a,aa - xx,aa - yy);
+		add(a,bb - xx,aa - yy);
+		add(a,aa - xx,bb - yy);
+		add(a,bb - xx,bb - yy);
+		int x=2;
+		add(a,aa - xx,aa - yy+x);
+		add(a,bb - xx,aa - yy+x);
+		add(a,aa - xx,bb - yy+x);
+		add(a,bb - xx,bb - yy+x);
+	}
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int a[MAX][MAX];
 	int count =MAX;
 	int stat;
 	int no;
 	stat=0;
+	srand (time(NULL));
 	for (int i = 0; i < count; ++i)
 	{
 		for (int j = 0; j < count; ++j)
@@ -143,9 +242,18 @@ int main(int argc, char **argv)
 		}
 	}
 	implant(a);
-
+	int i=0;
 	while(1){
-
+		i++;
+		if(i==100){
+			i=0;
+			int X=MAX-10;
+			add(a,1,X+7);
+			add(a,2,X+6);
+			add(a,3,X+6);
+			add(a,3,X+7);
+			add(a,3,X+8);
+		}
 	if(fork()==0){
 		execvp("clear",argv);
 		exit(1);
@@ -163,7 +271,6 @@ int main(int argc, char **argv)
 					a[i][j]=3; //almost dead :W
 				//else if dead, good
 			}
-			
 			else if(no==3){
 				if(a[i][j]==0)
 					pregnant(a,i,j);
@@ -173,7 +280,6 @@ int main(int argc, char **argv)
 				if(a[i][j]==1)
 					a[i][j]=3; //died of overcrowding
 			}
-			
 
 		}
 	}
@@ -185,7 +291,6 @@ int main(int argc, char **argv)
 	//view(a);
 	usleep(60000);
 	cout<<endl;
-	
 }
 	return 0;
 }
